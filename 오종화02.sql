1c538330e879bc7153c854ec744a282946011e14
nexiom1221 <49227888+nexiom1221@users.noreply.github.com>
2019-05-16 21:07:32 +0900

    Add files via upload

--- 오종화02.sql
+++ 오종화02.sql
@@ -46,21 +46,89 @@ EMPNO   ENAME  DEPTNO
  */
  
  -- 실습 9)
- SELECT e.empno,e.ename,e.sal,e.deptno
-   FROM emp e
-  WHERE sal<3000
+SELECT e.empno,e.ename,e.sal,e.deptno
+  FROM emp e
+ WHERE e.deptno=20 AND e.sal<3000
 ;
 /*
 EMPNO,  ENAME,  SAL,   DEPTNO
 7369	SMITH	800	    20
-7499	ALLEN	1600	30
-7521	WARD	1250	30
 7566	JONES	2975	20
-7654	MARTIN	1250	30
-7698	BLAKE	2850	30
-7782	CLARK	2450	10
-7844	TURNER	1500	30
-7900	JAMES	950   	30
-7934	MILLER	1300	10
 */
 
+-- 실습 10)
+SELECT   e.empno
+        ,e.ename
+        ,e.sal
+        ,e.comm
+        ,e.sal+e.comm as "sal+comm"
+  FROM  emp e
+;
+/*
+EMPNO,  ENAME,  SAL,   COMM, sal+comm
+7369	SMITH	800		
+7499	ALLEN	1600	300	    1900
+7521	WARD	1250	500	    1750
+7566	JONES	2975		
+7654	MARTIN	1250	1400	2650
+7698	BLAKE	2850		
+7782	CLARK	2450		
+7839	KING	5000		
+7844	TURNER	1500	0	    1500
+7900	JAMES	950		
+7902	FORD	3000		
+7934	MILLER	1300		
+*/
+
+--실습 11)
+SELECT   e.empno
+        ,e.ename
+        ,e.sal
+        ,e.comm
+        ,e.sal+e.comm*12 as 년급여
+  FROM  emp e
+;
+/*
+EMPNO, ENAME,   SAL,   COMM,  년급여
+7369	SMITH	800		
+7499	ALLEN	1600	300	  5200
+7521	WARD	1250	500	  7250
+7566	JONES	2975		
+7654	MARTIN	1250	1400  18050
+7698	BLAKE	2850		
+7782	CLARK	2450		
+7839	KING	5000		
+7844	TURNER	1500	0	  1500
+7900	JAMES	950		
+7902	FORD	3000		
+7934	MILLER	1300
+*/
+
+-- 실습 12)
+SELECT   e.empno
+        ,e.ename
+        ,e.job
+        ,e.sal
+        ,e.comm
+  FROM  emp e
+ WHERE  e.ename='MARTIN' OR e.ename='BLAKE'
+;
+/*
+EMPNO,  ENAME,   JOB,       SAL,    COMM
+7654	MARTIN	SALESMAN	1250	1400
+7698	BLAKE	MANAGER	    2850
+*/
+
+-- 실습 13)
+SELECT   e.empno
+        ,e.ename
+        ,e.job
+        ,e.sal+e.comm as "급여+수당"
+  FROM  emp e
+ WHERE  e.ename='MARTIN' OR e.ename='BLAKE'
+;
+/*
+EMPNO,  ENAME,   JOB,       급여+수당
+7654	MARTIN	SALESMAN	2650
+7698	BLAKE	MANAGER
+*/
